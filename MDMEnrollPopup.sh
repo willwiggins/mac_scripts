b219db9bb32cc3e9e71c401d63789d27d5a258db
Will Wiggins <willwiggins@users.noreply.github.com>
2016-08-03 12:08:51 -0400

    Update MDMEnrollPopup.sh

--- MDMEnrollPopup.sh
+++ MDMEnrollPopup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+#Replace yourdomain on line 8 with your open enrollment link
+
 if ( /usr/bin/profiles -P | /usr/bin/grep -q bushel ); then
   echo "Profile Exists"
 else
-  open -a safari https://sl.bushel.com
+  open -a safari https://yourdomain.bushel.com
 fi
