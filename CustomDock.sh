d20fcc07c21188a462a812556cca0f99c7bd7cea
Will Wiggins <willwiggins@users.noreply.github.com>
2018-07-16 11:31:15 -0400

    Update CustomDock.sh

--- CustomDock.sh
+++ CustomDock.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-DOCKUTIL=/usr/local/bin/dockutil
+sleep 10
 
-$DOCKUTIL --remove all --no-restart
-$DOCKUTIL --add '/Applications/Launchpad.app' --no-restart
-$DOCKUTIL --add '/Applications/Google Chrome.app' --no-restart
-$DOCKUTIL --add '/Applications/Slack.app' --no-restart
+LoggedInUser=$3
+LoggedInUserHome="/Users/$3"
+plist=$LoggedInUserHome/Library/Preferences/com.apple.dock.plist
+
+echo "Logged in user is $LoggedInUser"
+echo "Logged in user's home $LoggedInUserHome"
+
+
+DOCKUTIL=/Library/Scripts/SalesLoft/dockutil
+
+$DOCKUTIL --remove all --no-restart $LoggedInUserHome
+$DOCKUTIL --add '/Applications/Launchpad.app' --no-restart $LoggedInUserHome
+$DOCKUTIL --add '/Applications/Google Chrome.app' --no-restart $LoggedInUserHome
+$DOCKUTIL --add '/Applications/Slack.app' --no-restart $LoggedInUserHome
+$DOCKUTIL --add '/Applications/Self Service.app' --no-restart $LoggedInUserHome
 $DOCKUTIL --add '~/Downloads' --view grid --display folder --allhomes
