35c67b042031e1df9d6dc35be225a84f05f00938
Will Wiggins <willwiggins@users.noreply.github.com>
2020-10-05 09:46:50 -0400

    Update available_gsuite_licenses.sh

--- available_gsuite_licenses.sh
+++ available_gsuite_licenses.sh
@@ -1,4 +1,5 @@
 # This script uses GAM to check on current G-Suite Enterprise License usage and will alert via Slack Webhook if the count is under the set threshold
+# I am personally using this script as a LaunchDaemon to check every so often automatically so I don't have to keep a close eye on licensing.
 
 #!/bin/bash
 # Sets GAM Path shortcut to default location.  Will want to change this to an exact location if running via a service
